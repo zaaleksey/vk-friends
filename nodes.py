e62872dacc059ff13902b5a1bd0fa46b05f48ff5
AlexeyZavarzin <alex.zavr.27100@mail.ru>
2021-08-22 20:57:16 +0400

    nodes

---
+++ nodes.py
@@ -0,0 +1,27 @@
+import json
+import os
+from os.path import exists
+
+
+def forming_nodes_into_file():
+    path_file = "csv_files/nodes.csv"
+
+    if exists(path_file):
+        os.remove(path_file)
+
+    users_files = os.listdir("users")
+    with open(path_file, "w") as nodes_file:
+        nodes_file.write("id, label, sex\n")
+        for user_file in users_files:
+            with open(f"users/{user_file}", "r") as data_file:
+                user = json.load(data_file)
+                try:
+                    line = f"{user['id']}, {user['first_name']} {user['last_name']}, {user['sex']}\n"
+                    nodes_file.write(line)
+                except ValueError:
+                    print(line)
+                    continue
+
+
+if __name__ == "__main__":
+    forming_nodes_into_file()
--- save_user_json.py
+++ save_user_json.py
@@ -8,29 +8,43 @@ from secret import TOKEN_SERVICE, MY_ID
 
 
 def get_json_friends_by_id(user_id):
-    url = f"https://api.vk.com/method/friends.get?user_id={user_id}&fields=nickname, domain, sex, bdate, city, " \
-          f"country, timezone, has_mobile, contacts, education, online, relation, last_seen, status, " \
-          f"can_write_private_message, can_see_all_posts, can_post, universities&access_token={TOKEN_SERVICE}&v=5.131 "
+    url = (
+        f"https://api.vk.com/method/friends.get?user_id={user_id}&fields=nickname, domain, sex, bdate, city, "
+        f"country, timezone, has_mobile, contacts, education, online, relation, last_seen, status, "
+        f"can_write_private_message, can_see_all_posts, can_post, universities&access_token={TOKEN_SERVICE}&v=5.131 "
+    )
     return requests.get(url).json()
 
 
+def writing_users_to_file(user):
+    filename = f"users/{user['id']}.json"
+    if exists(filename):
+        return
+
+    with open(filename, "w+") as user_file:
+        json.dump(user, user_file, ensure_ascii=True, indent=4)
+
+
 def save_users_json():
-    count = 0
     friends = get_json_friends_by_id(MY_ID)
+    counter, total_friends = 0, friends["response"]["count"]
     for friend in friends["response"]["items"]:
-        friend_file = open(f"users/{friend['id']}.json", "w+")
-        json.dump(friend, friend_file, ensure_ascii=True, indent=4)
+
+        if "deactivated" in friend.keys():
+            continue
+
         if not friend["is_closed"]:
+            writing_users_to_file(friend)
             users = get_json_friends_by_id(friend["id"])
             for user in users["response"]["items"]:
-                if not exists(f"users/{user['id']}.json"):
-                    user_file = open(f"users/{user['id']}.json", "w+")
-                    json.dump(user, user_file, ensure_ascii=True, indent=4)
-            sleep(.5)
-        count += 1
-        print(f"{count}/{friends['response']['count']}")
+                writing_users_to_file(user)
+            sleep(0.5)
+        counter += 1
+        print(
+            f"{counter}/{total_friends}: {friend['first_name']} {friend['last_name']}"
+        )
 
 
-if __name__ == '__main__':
-    # save_users_json()
-    pass
+if __name__ == "__main__":
+    # run once
+    save_users_json()
